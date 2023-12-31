fd744b9a44b58dcf94234c45a29d621df0b099ea
Andrew Fasano <fasano@mit.edu>
2023-12-30 20:45:34 -0800

    Discard impossible states (multi-solns)

--- solve.py
+++ solve.py
@@ -16,6 +16,14 @@ all verifiers return True in the fewest number of guesses.
 
 NUM_DIGITS = 3
 
+from itertools import product
+
+def generate_selection_flag_combinations(selection_flags):
+    # Generate all combinations of selection flags (one from each verifier)
+    verifiers = list(selection_flags.keys())
+    flag_combinations = product(*(selection_flags[v] for v in verifiers))
+    return flag_combinations
+
 class Verifier():
     '''
     Base class for a verifier. 
@@ -174,14 +182,18 @@ class Game():
             print("-"*30)
             concrete_guess, solved = self.guess_loop()
 
+            all_true = False
             if not solved:
                 verifier_output = {v: v(concrete_guess) for v in self.verifiers}
                 print(f"Guess {i+1}: {concrete_guess} returns")
+                all_true = True
+
                 for v, output in verifier_output.items():
                     print(f"\t{v.get_public_state()}: {output}")
+                    all_true &= output
                 self.results[concrete_guess] = verifier_output
 
-            else:
+            if solved or all_true:
                 print("*"*30)
                 print(f"Alleged solution is {concrete_guess} after {i+1} guesses!")
                 print("Results:")
@@ -280,6 +292,36 @@ class Game():
             selection_possibilities[v] = len(possibilities)
             for idx, (sel, opt) in enumerate(zip(selection_flags[v], possibilities)):
                 s.add(z3.Implies(sel, opt))
+
+        # Generate combinations
+        flag_combinations = generate_selection_flag_combinations(selection_flags)
+
+        # Now iterate over these combinations and test each one
+        for combination in flag_combinations:
+            s_test = z3.Solver()
+            # Add all existing constraints
+            for c in s.assertions():
+                s_test.add(c)
+            # Add the specific combination of selection flags
+            for flag in combination:
+                s_test.add(flag)
+
+            if s_test.check() == z3.unsat:
+                s.add(z3.Not(z3.And(*[combination])))
+                continue
+
+            # Get model, add constraint that we can't use this again
+            m = s_test.model()
+            first_solution = [m[guess[digit]].as_long() for digit in range(NUM_DIGITS)]
+            s_test.add(z3.Or([guess[digit] != first_solution[digit] for digit in range(NUM_DIGITS)]))
+
+            # Check for another solution
+            if s_test.check() == z3.sat:
+                #print("Multiple solutions found for:", combination)
+                s.add(z3.Not(z3.And(*[combination])))
+
+        # With the correct set of selection flags (i.e., one of each), only a single solution would be possible.
+        # This doesn't express well in boolean logic. Instead let's enumerate and test
 
         if len(self.results) > 0:
             # If we had any prior results, filter our selection flags to remove any that we now know are impossible
